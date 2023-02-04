6aa6f0bf293af28b9c9cfe3c94d36106615d0dd5
Samy Metadjer <samy.metadjer@dashlane.com>
2023-02-05 00:29:15 +0100

    refactor route to use handler

--- app/src/routes/globalPriceIndex.ts
+++ app/src/routes/globalPriceIndex.ts
@@ -1,21 +1,29 @@
 import * as express from "express";
-import { getGlobalPriceIndex } from "../modules/orderBook/getGlobalPriceIndex";
+import {
+  getGlobalPriceIndex,
+  GlobalPriceIndexParameters,
+} from "../modules/orderBook";
 
 const router = express.Router();
 
-/* GET global price index based on Binance / Kraken / Huobi. */
-router.get("/", (_req: express.Request, res: express.Response) => {
-  getGlobalPriceIndex((error, globalPriceIndex) => {
+/* Handler - GET global price index based on Binance / Kraken / Huobi. */
+const handler = (req: express.Request, res: express.Response, next: any) => {
+  const params: GlobalPriceIndexParameters = {
+    symbol: req.query.symbol as string,
+    limit: Number(req.query.limit),
+  };
+  getGlobalPriceIndex(params, (error, globalPriceIndex) => {
     if (error) {
       console.log("An error happened");
-      console.log(error);
-      res.send(error);
+      next(error);
       return;
     }
     console.log("Global price index: ", globalPriceIndex);
     res.send({ globalPriceIndex });
     return;
   });
-});
+};
+
+router.get("/", handler);
 
 module.exports = router;
